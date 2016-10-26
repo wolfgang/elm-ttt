f0b358e4106fa343bd27fbd4e4fb1939bd558915
wolfgang <wolfgang.deutsch@gmail.com>
2016-10-26 20:14:55 +0200

    Add alternative implementation using recursion for early exit

--- src/Cell.elm
+++ src/Cell.elm
@@ -2,6 +2,23 @@ module Cell exposing (setHighlightedCell)
 import Model exposing (Model)
 import Board exposing (CellRect)
 
+setHighlightedCell : Model -> Model
+setHighlightedCell model =
+    List.foldr 
+        setHighlightedCellAt
+        model 
+        Board.cellCoords
+--  setHighlightedCell2 Board.cellCoords model
+
+setHighlightedCellAt : (Int, Int) -> Model -> Model
+setHighlightedCellAt cellCoords model =
+    let 
+        cellRect = Board.getCellRectAt cellCoords model
+    in
+        if cellContainsPoint cellRect model.mousePosition then
+            { model | highlightedCell = Just cellCoords }
+        else
+            model
 
 cellContainsPoint : CellRect -> (Float, Float) -> Bool
 cellContainsPoint cell (x, y) = 
@@ -14,19 +31,22 @@ cellContainsPoint cell (x, y) =
         y >= cellY &&
         y <= cellY + cellSize
 
-setHighlightedCellAt : (Int, Int) -> Model -> Model
-setHighlightedCellAt cellCoords model =
-    let 
-        cellRect = Board.getCellRectAt cellCoords model
-    in
-        if cellContainsPoint cellRect model.mousePosition then
-            { model | highlightedCell = Just cellCoords }
-        else
-            model
 
-setHighlightedCell : Model -> Model
-setHighlightedCell model =
-    List.foldr 
-        setHighlightedCellAt
-        model 
-        Board.cellCoords
+-- Alternative implementation using recursion for early exit
+
+setHighlightedCell2 : List (Int, Int) -> Model -> Model
+setHighlightedCell2 cellCoords model =
+    case List.head cellCoords of
+        Nothing -> model
+        Just coord -> 
+            let cellRect = Board.getCellRectAt coord model
+            in
+                if cellContainsPoint cellRect model.mousePosition then
+                    { model | highlightedCell = Just coord }
+                else
+                    setHighlightedCell2 (tailOrEmptyList cellCoords) model
+
+tailOrEmptyList list = 
+    case List.tail list of
+        Nothing -> []
+        Just tail -> tail
